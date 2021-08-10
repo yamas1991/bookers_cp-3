0465d002bb63f1d18fef9fb6f9636102ae90b197
EC2 Default User <ec2-user@ip-172-31-67-223.ec2.internal>
2021-08-10 14:08:31 +0000

    bookers hidoukituusin

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get '/home/about' => 'homes#about'
   resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
-    resources :likes, only: [:create, :destroy]
+    resource :likes, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
   resources :users, only: [:index, :show, :edit, :update]
